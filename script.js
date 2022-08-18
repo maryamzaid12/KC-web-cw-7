a2237df1eb35e04e144b7fb4ee6db435a91e7ee0
maryamzaid12 <smem20066@gmail.com>
2022-08-18 21:48:13 +0300

    cw7

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function BMI (weight,height) {
+ let result =  weight / (height * height);
+ return result;
+}
+    
+console.log(BMI(70,1.7) );
+function Status (BMI) {
 
+   if (bmi <18.5) {
+        return"لديك نقص في الوزن";
+   } else if ( bmi >=18.5 && bmi<25){
+        return"وزنك صحي" ;
+   } else if (bmi>=25) {
+      return"لديك زيادة في الوزن";
+   }
+}
+function calculate () {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let bmi = bmi (weight,height);
+   let desc = Status (bmi);
+    let div = document.getElementByld("result");
+    div.innerText = BMI + " == " + desc;
+}
